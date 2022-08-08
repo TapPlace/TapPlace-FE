460747f4306047d6c59213bff1cfff7cff5fa29e
Limttugi <junhuk820@gmail.com>
2022-08-08 17:58:36 +0900

    ServicePage main2 CSS Complete

--- src/pages/Service.tsx
+++ src/pages/Service.tsx
@@ -5,7 +5,7 @@ import '../style/pages/Service.scss';
 
 function Service() {
   return (
-    <div id="serviceContainer">
+    <>
       <header id="serviceHeader">
         <h1 id="title">Tap Place</h1>
         <nav id="serviceNav">
@@ -29,11 +29,11 @@ function Service() {
         <div id="main1" className="mainDivideContainer">
           <div id="main1Container">
             <article className="contentContainer">
-              <h1 id="main1_line1">
+              <h1 id="main1_line1" className="line1">
                 간편결제 가맹점 확인
                 <br /> 한곳에서 간편하게
               </h1>
-              <p id="main1_line2">
+              <p id="main1_line2" className="line2">
                 힘들게 찾아다닐 필요없이 한번에 확인할 수 있어요
                 <br />
                 탭플레이스에서 내 주변 간편결제 가맹점을 확인해보세요
@@ -61,35 +61,43 @@ function Service() {
             </article>
             <img
               id="main1_mokupImg"
+              className="mokupImg"
               src={require('../img/mokup-1.png')}
               alt="mokup"
             />
           </div>
         </div>
         <div id="main2" className="mainDivideContainer">
-          <h4>
+          <h4 id="main2_lineTop">
             탭플레이스와 함께라면 이 모든 간편결제 가맹점을 쉽게 확인할 수
             있어요
           </h4>
-          <div id="main2LogoConainter">
+          <div id="main2LogoContainer">
             <img src={require('../img/visa.png')} alt="visaLogo" />
             <img src={require('../img/payco.png')} alt="paycoLogo" />
-            <img src={require('../img/appleLogo.png')} alt="appleLogo" />
+            <img src={require('../img/apple.png')} alt="appleLogo" />
             <img src={require('../img/zero.png')} alt="zeroLogo" />
             <img src={require('../img/google.png')} alt="googleLogo" />
             <img src={require('../img/kakao.png')} alt="kakaoLogo" />
             <img src={require('../img/naver.png')} alt="naverLogo" />
             <img src={require('../img/master.png')} alt="masterLogo" />
           </div>
-          <img src={require('../img/mokup-2.png')} alt="mokup-2" />
-          <article className="contentContainer">
-            <img className="applicationLogo" src="" alt="" />
-            <h1>
+          <img
+            id="main2_mokupImg"
+            className="mokupImg"
+            src={require('../img/mokup-2.png')}
+            alt="mokup-2"
+          />
+          <article id="main2contentContainer" className="contentContainer">
+            <div id="appLogoContainer">
+              <img className="applicationLogo" src="" alt="" />
+            </div>
+            <h1 id="main2_line1" className="line1">
               내 주변 간편결제 가맹점을
               <br />
               한눈에 확인하세요
             </h1>
-            <p>
+            <p id="main2_line2" className="line2">
               이제 간편결제 안될까봐 걱정하지 마세요
               <br />
               결제 가능여부를 미리 알려드릴께요.
@@ -110,7 +118,7 @@ function Service() {
         </div>
       </main>
       <footer id="footer">푸터</footer>
-    </div>
+    </>
   );
 }
 
