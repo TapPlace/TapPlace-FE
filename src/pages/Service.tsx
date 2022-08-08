f985b04b53f2ce5fbd171f470e5e7ef138534e9f
Limttugi <junhuk820@gmail.com>
2022-08-08 17:02:41 +0900

    ServicePage main1 CSS modified

--- src/pages/Service.tsx
+++ src/pages/Service.tsx
@@ -29,14 +29,14 @@ function Service() {
         <div id="main1" className="mainDivideContainer">
           <div id="main1Container">
             <article className="contentContainer">
-              <h1>
+              <h1 id="main1_line1">
                 간편결제 가맹점 확인
                 <br /> 한곳에서 간편하게
               </h1>
-              <p>
-                힘들게 찾아다닐 필요없이 한번에 확인할 수 있어요.
+              <p id="main1_line2">
+                힘들게 찾아다닐 필요없이 한번에 확인할 수 있어요
                 <br />
-                탭플레이스에서 내 주변 간편결제 가맹점을 확인해보세요.
+                탭플레이스에서 내 주변 간편결제 가맹점을 확인해보세요
               </p>
               <ul id="logoContainer">
                 <li>
@@ -66,11 +66,46 @@ function Service() {
             />
           </div>
         </div>
-        <div id="main2" className="mainDivideContainer"></div>
+        <div id="main2" className="mainDivideContainer">
+          <h4>
+            탭플레이스와 함께라면 이 모든 간편결제 가맹점을 쉽게 확인할 수
+            있어요
+          </h4>
+          <div id="main2LogoConainter">
+            <img src={require('../img/visa.png')} alt="visaLogo" />
+            <img src={require('../img/payco.png')} alt="paycoLogo" />
+            <img src={require('../img/appleLogo.png')} alt="appleLogo" />
+            <img src={require('../img/zero.png')} alt="zeroLogo" />
+            <img src={require('../img/google.png')} alt="googleLogo" />
+            <img src={require('../img/kakao.png')} alt="kakaoLogo" />
+            <img src={require('../img/naver.png')} alt="naverLogo" />
+            <img src={require('../img/master.png')} alt="masterLogo" />
+          </div>
+          <img src={require('../img/mokup-2.png')} alt="mokup-2" />
+          <article className="contentContainer">
+            <img className="applicationLogo" src="" alt="" />
+            <h1>
+              내 주변 간편결제 가맹점을
+              <br />
+              한눈에 확인하세요
+            </h1>
+            <p>
+              이제 간편결제 안될까봐 걱정하지 마세요
+              <br />
+              결제 가능여부를 미리 알려드릴께요.
+            </p>
+          </article>
+        </div>
         <div id="main3" className="mainDivideContainer">
           메인
         </div>
         <div id="main4" className="mainDivideContainer">
+          메인
+        </div>
+        <div id="main5" className="mainDivideContainer">
+          메인
+        </div>
+        <div id="main6" className="mainDivideContainer">
           메인
         </div>
       </main>
