324b77580564612199a2c791c06d4aab222c6c58
Limttugi <junhuk820@gmail.com>
2022-08-17 03:04:49 +0900

    Add MemberSlide ScrollEvent in ServicePage

--- src/pages/Service.tsx
+++ src/pages/Service.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PayLogoSlider from '../components/PayLogoSlider';
 import StoreButton from '../components/StoreButton';
 
