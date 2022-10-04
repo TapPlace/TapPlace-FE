2c44f6c20daa65429e7f2e7ebbb7342342387a96
Limttugi <junhuk820@gmail.com>
2022-10-04 17:33:48 +0900

    Add OverlapMarkerCode

--- src/components/appService/NaverMap.tsx
+++ src/components/appService/NaverMap.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import {
   setDetail,
@@ -14,6 +14,8 @@ function NaverMap(props: any) {
   const { myLocation, lastLocation, markers, detailFlag } = useAppSelector(
     state => state.naver,
   );
+  const [naverMap, setNaverMap]: any = useState();
+  let overMarkers: any = [];
 
   // 네이버 지도 띄우기
   async function naverFunction() {
@@ -34,6 +36,8 @@ function NaverMap(props: any) {
         zoom: 16,
         zoomControl: false,
       });
+      // 맵 저장
+      await setNaverMap(map);
       // map 객체 저장해서 하위 컴포넌트로 보내기
       const lowComponent = (map: any) => {
         return props.propFunction(map);
@@ -61,6 +65,10 @@ function NaverMap(props: any) {
         strokeColor: 'rgba(78, 119, 251, 0.5)',
         strokeWeight: 1.5,
       });
+      // 지도 줌 바뀔 시 겹침 마커 처리
+      naver.maps.Event.addListener(map, 'zoom_changed', function () {
+        updateMarkersIntersectState();
+      });
     }
   }
   // distance 마커 표시
@@ -79,6 +87,7 @@ function NaverMap(props: any) {
             anchor: new naver.maps.Point(18, 30),
           },
         });
+        overMarkers.push(otherMarkers);
         markerClickEvent({ map, otherMarkers, i, imgSrc, category });
       }
       if (markers[i].category_group_name === '편의점') {
@@ -93,6 +102,7 @@ function NaverMap(props: any) {
             // anchor: new naver.maps.Point(32, 30),
           },
         });
+        overMarkers.push(otherMarkers);
         markerClickEvent({ map, otherMarkers, i, imgSrc, category });
       }
       if (markers[i].category_group_name === '카페') {
@@ -106,6 +116,7 @@ function NaverMap(props: any) {
               '<div style="text-align: center;"><img src="/img/AppPage/Marker/cafe.png" alt="markerImg" /><p style="text-shadow: 1px 0 white, 1px 0 white, 1px 0 white, 1px 0 white; font-size: 13px;">카페</p></div>',
           },
         });
+        overMarkers.push(otherMarkers);
         markerClickEvent({ map, otherMarkers, i, imgSrc, category });
       }
       if (markers[i].category_group_name === '마트') {
@@ -169,6 +180,167 @@ function NaverMap(props: any) {
         dispatch(setDetail(markers[i]));
       }
     });
+  }
+  // 겹침 마커 처리
+  let rectangles: any = [];
+  let markersMap: never;
+  function updateMarkersIntersectState() {
+    console.log(overMarkers);
+    let store: any = getIntersectMarkerStore(overMarkers);
+    console.log(store);
+    let intersectMarkers: any = getMarkersByStore(store);
+    let boundsList: any = getIntersectMarkerBounds(intersectMarkers);
+    resetMarkers(overMarkers);
+    highlightMarkers(intersectMarkers);
+
+    resetRectangles(rectangles);
+    rectangles = drawRectangles(boundsList);
+  }
+  function getIntersectMarkerStore(overMarkers: any) {
+    let store: any = [];
+    let target: any, checked, targetBounds, checkedBounds;
+
+    for (let i = 0; i < overMarkers.length; i++) {
+      target = overMarkers[i];
+      for (var j = 0; j < overMarkers.length; j++) {
+        checked = overMarkers[j];
+        if (target === checked) continue;
+        targetBounds = target.getDrawingRect();
+        checkedBounds = checked.getDrawingRect();
+        if (targetBounds.intersects(checkedBounds)) {
+          _inertToIntersectStore(store, target.getTitle, checked.getTitle);
+        }
+      }
+    }
+    return store;
+  }
+  function _inertToIntersectStore(store: any, value1: any, value2: any) {
+    let index1 = _has(store, value1);
+    console.log('index1');
+    console.log(index1);
+    let index2 = _has(store, value2);
+    if (index1 !== -1 && index2 === -1) {
+      index2 = index1;
+    } else if (index1 === -1 && index2 !== -1) {
+      index1 = index2;
+    }
+    if (index1 === -1 && index2 === -1) {
+      store.push([value1, value2]);
+    } else {
+      store[index1].push(value1);
+      store[index2].push(value2);
+
+      if (index1 !== index2) {
+        let low = Math.min(index1, index2);
+        let high = Math.max(index1, index2);
+
+        store[low] = store[low].concat(store[high]);
+        store.splice(high, 1);
+        store[low] = _uniqueArray(store[low]);
+      } else {
+        store[index1] = _uniqueArray(store[index1]);
+      }
+    }
+  }
+  function _has(store: any, value: any) {
+    var array;
+    for (var i = 0; i < store.length; i++) {
+      array = store[i];
+      if (array.indexOf(value) !== -1) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+  function _uniqueArray(a: any) {
+    return a.reduce(function (p: any, c: any) {
+      if (p.indexOf(c) < 0) p.push(c);
+      return p;
+    }, []);
+  }
+  function resetRectangles(rectangles: any) {
+    for (let i = 0; i < rectangles.length; i++) {
+      rectangles[i].setMap(null);
+    }
+  }
+  function drawRectangles(boundsList: any) {
+    let rectangles = [];
+    for (let i = 0; i < boundsList.length; i++) {
+      rectangles.push(
+        new naver.maps.Rectangle({
+          map: naverMap,
+          bounds: boundsList[i],
+          strokeColor: 'black',
+          strokeWeight: 2,
+          strokeOpacity: 0.8,
+          fillColor: 'black',
+          fillOpacity: 0.4,
+        }),
+      );
+    }
+    return rectangles;
+  }
+  function getMarkersByStore(store: any) {
+    let intersectMarkers: any = [];
+    for (let i = 0; i < store.length; i++) {
+      intersectMarkers[i] = [];
+      for (let j = 0; j < store[i].length; j++) {
+        intersectMarkers[i].push(markersMap[store[i][j]]);
+      }
+    }
+
+    return intersectMarkers;
+  }
+  function getIntersectMarkerBounds(intersectMarkers: any) {
+    let boundsList = [],
+      bounds: any;
+
+    for (var i = 0; i < intersectMarkers.length; i++) {
+      for (var j = 0; j < intersectMarkers[i].length; j++) {
+        if (j === 0) {
+          bounds = intersectMarkers[i][j].getDrawingRect();
+        } else {
+          bounds = bounds.union(intersectMarkers[i][j].getDrawingRect());
+        }
+      }
+
+      bounds = _pixelBoundsToLatLngBounds(naverMap, bounds);
+      boundsList.push(bounds);
+    }
+
+    return boundsList;
+  }
+  function resetMarkers(overMarkers: any) {
+    for (var i = 0; i < overMarkers.length; i++) {
+      var icon = overMarkers[i].getIcon();
+      // icon.url = MARKER_ICON_URL;
+      overMarkers[i].setIcon(icon);
+    }
+  }
+  function highlightMarkers(intersectMarkers: any) {
+    for (var i = 0; i < intersectMarkers.length; i++) {
+      for (var j = 0; j < intersectMarkers[i].length; j++) {
+        var icon = intersectMarkers[i][j].getIcon();
+        // icon.url = MARKER_HIGHLIGHT_ICON_URL;
+        intersectMarkers[i][j].setIcon(icon);
+      }
+    }
+  }
+  function _pixelBoundsToLatLngBounds(map: any, pixelBounds: any) {
+    var zoom = map.getZoom();
+    var proj = map.getProjection();
+
+    var min = pixelBounds.getMin();
+    var max = pixelBounds.getMax();
+
+    min = proj.scaleDown(min, zoom);
+    max = proj.scaleDown(max, zoom);
+
+    min = proj.fromPointToCoord(min);
+    max = proj.fromPointToCoord(max);
+
+    return new naver.maps.LatLngBounds(min, max);
   }
 
   // 처음 지도 생성 + 마커가 있을 시 지도 생성
